92cfdf5f1ac457c8812c5b59ea793f6602afa124
Liza Medvedeva <medvedeva_elizaveta2000@mail.ru>
2020-02-08 21:00:00 +0300

    Add animation for menu

--- bcomp-js/src/main/angular/src/app/app.component.ts
+++ bcomp-js/src/main/angular/src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('showHideMenu', [
+      state('show', style({
+      left: '0'
+      })),
+      state('hide', style({
+        left: '-230px'
+      })),
+      transition('show => hide', [
+        animate('0.4s')
+      ]),
+      transition('hide => show', [
+        animate('0.4s')
+      ]),
+    ]),
+    trigger('showHideContent', [
+      state('show', style({
+        marginLeft: '230px',
+      })),
+      state('hide', style({
+        marginLeft: 'unset',
+      })),
+      transition('show => hide', [
+        animate('0.4s')
+      ]),
+      transition('hide => show', [
+        animate('0.4s')
+      ]),
+    ]),
+    trigger('showHideButton', [
+      state('show', style({
+        width: 0,
+        height: 0,
+        borderRight: '40px solid rgba(6, 6, 6, 0.7)',
+      })),
+      state('hide', style({
+        width: 0,
+        height: 0,
+        borderLeft: '40px solid rgba(6, 6, 6, 0.7)',
+        borderRight: 'none'
+      })),
+      transition('show => hide', [
+        animate('0.4s')
+      ]),
+      transition('hide => show', [
+        animate('0.4s')
+      ]),
+    ])
+  ]
 })
 export class AppComponent {
   public loading: boolean = false;
